d8207f959012a8eebe03a9790f01fd263146381f
dvdknstmnn <dvdknstmnn@gmail.com>
2019-03-12 14:15:02 -0500

    XCode Project

---
+++ PocketChoir/PocketChoir/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PocketChoir
+//
+//  Created by David on 3/12/19.
+//  Copyright © 2019 Pocket Choir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
